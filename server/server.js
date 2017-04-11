545facebcae4949bf93e343af655a52cbd5eaee9
Dallin Parker <dallin.r.parker@gmail.com>
2017-04-10 22:38:55 -0600

    added backend

---
+++ server/server.js
@@ -0,0 +1,32 @@
+require('dotenv').config();
+const cors = require('cors');
+const path = require('path');
+const express = require('express');
+const massive = require('massive');
+const passport = require('passport');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+
+// INITIATE EXPRESS APP & SET LISTENING PORT ================
+const app = module.exports = express();
+app.set('port', process.env.PORT || 3000);
+
+// MIDDLEWARE FOR EVERYTHING TO PASS THROUGH ================
+// app.use(bodyPaser.json());
+// app.use(express.static(`${__dirname}./../public`));
+// app.use(cors());
+// app.use(session({
+//     secret: process.env.SESSION_SECRET,
+//     saveUninitialized: false,
+//     resave: false
+// }));
+// app.use(passport.initialize());
+// app.use(passport.session());
+
+
+
+
+// LISTENING ON PORT ===============================
+app.listen(app.get('port'), () => {
+    console.log('Vestid is running on port', app.get('port'));
+});
