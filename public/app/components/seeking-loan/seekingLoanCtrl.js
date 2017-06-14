fc9c608f12d592d6fa9170914f2105472c20a9d3
Dallin Parker <dallin.r.parker@gmail.com>
2017-06-13 22:29:52 -0600

    seeking loan form is almost done

--- public/app/components/seeking-loan/seekingLoanCtrl.js
+++ public/app/components/seeking-loan/seekingLoanCtrl.js
@@ -1,3 +1,24 @@
-angular.module('vestid').controller('seekingLoanCtrl', () => {
+angular.module('vestid').controller('seekingLoanCtrl', ($scope) => {
+
+	$scope.loans = [
+		{ price: 500 },
+		{ price: 750 },
+		{ price: 1000 },
+		{ price: 1500 },
+		{ price: 2000 }
+	];
+
+	$scope.businessTypes = [
+		{ type: 'Startup' },
+		{ type: 'Tech' },
+		{ type: 'Food' },
+		{ type: 'Creative' },
+		{ type: 'Retail' }
+
+	]
+
+	$scope.slform = (slf) => {
+		console.log(slf)
+	}
 
 });
