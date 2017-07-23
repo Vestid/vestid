78cf88fa7a941860e1e80262d9ba8de97207045b
Dallin Parker <dallin.r.parker@gmail.com>
2017-07-22 19:26:09 -0600

    added logic for ng-if for offering-loan

--- public/app/components/offering-loan/offeringLoanCtrl.js
+++ public/app/components/offering-loan/offeringLoanCtrl.js
@@ -1,4 +1,10 @@
-angular.module("vestid").controller("offeringLoanCtrl", ($scope) => {
+angular.module("vestid").controller("offeringLoanCtrl", ($scope, authService) => {
+
+	authService.currentUser().then(({data}) => {
+		(data === 'Unauthorized') ? $scope.authorized = false : $scope.authorized = true;
+		(data === 'Unauthorized') ? $scope.unauthorized = true : $scope.unauthorized = false;
+	})
+
 	$scope.businessTypes = [
 		{ type: 'Startup' },
 		{ type: 'Tech' },
