3a1bb4b96092f98416561fcd137def94b2ceb184
Dallin Parker <dallin.r.parker@gmail.com>
2017-09-14 00:11:39 -0400

    offering loan logic is working properly

--- public/app/components/seeking-loan/seekingLoanCtrl.js
+++ public/app/components/seeking-loan/seekingLoanCtrl.js
@@ -1,14 +1,15 @@
 angular.module('vestid').controller('seekingLoanCtrl',
 	['$scope', 'authService', 'prevState', '$state', ($scope, authService, prevState, $state) => {
 
+		const url = $state.href($state.current.name)
+		prevState.parseState(url)
+
 	authService.currentUser().then(user => {
 		(user.data === 'Unauthorized') ? $scope.authorized = false : $scope.authorized = true;
 		(user.data === 'Unauthorized') ? $scope.unauthorized = true : $scope.unauthorized = false;
 		($scope.authorized) ? $scope.user = user.data.firstname : $scope.user = null;
 	})
 
-	const url = $state.href($state.current.name)
-	prevState.parseState(url)
 
 
 	$scope.loans = [
