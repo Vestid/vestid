848adaf41baddcabc01cf0ac9064c01587b78934
Dallin Parker <dallin.r.parker@gmail.com>
2017-09-14 11:29:04 -0400

    working style

--- public/app/components/loan-list/loanListCtrl.js
+++ public/app/components/loan-list/loanListCtrl.js
@@ -2,4 +2,9 @@ angular.module('vestid').controller('loanListCtrl',
 	["$scope",($scope) => {
 
 
+	$scope.loans = [
+		{id: 1, type: 'Plumbing', name: "Ralph's Plumbing", loanAmount: 3400},
+		{id: 2, type: 'Start-up', name: "Vestid", loanAmount: 1350}
+		]
+
 }]);
