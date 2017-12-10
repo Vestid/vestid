60187ea538097b36132848e7fdf9ee0374fac37e
Dallin Parker <dallin.r.parker@gmail.com>
2017-12-10 01:06:52 -0500

    adding in logic for email template

--- server/sendgrid/index.js
+++ server/sendgrid/index.js
@@ -4,14 +4,15 @@ const sendgrid = require('@sendgrid/mail');
 sendgrid.setApiKey(`${SENDGRID_API}`);
 
 exports.sendReset = (req, res, next) => {
-    const { email } = req.body
+    const { id, firstname, lastname, email } = req.body.confirmed[0]
+
     const url = 'dallinparker.me'
     const msg = {
         to: email,
         from: 'vestid.email@gmail.com',
         subject: 'Vestid Support: Password Reset Request',
-        text: 'hi dallin, you are getting this email because you have requested to reset your password by clicking: dallinparker.me',
-        html: `<strong>hi dallin, you're getting this email because you've requested to reset your password by clicking: ${url}</stong>`,
+        text:`hi ${firstname}, you are getting this email because you have requested to reset your password by clicking: dallinparker.me`,
+        html: `<strong>hi ${firstname}, you're getting this email because you've requested to reset your password by clicking: ${url}</stong>`,
     };
     sendgrid.send(msg);
 };
