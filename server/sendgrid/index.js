5eab2ce80245e763f55968b3844dbb82daa31549
Dallin Parker <dallin.r.parker@gmail.com>
2017-12-10 01:13:38 -0500

    WIP 80% DONE

--- server/sendgrid/index.js
+++ server/sendgrid/index.js
@@ -4,11 +4,14 @@ const sendgrid = require('@sendgrid/mail');
 sendgrid.setApiKey(`${SENDGRID_API}`);
 
 exports.sendReset = (req, res, next) => {
+    //TODO: CREATE HELPER FUNCTION FOR MAKING THIS CLEANER
     const { id, firstname, lastname, email } = req.body.confirmed[0]
-
+    console.log('email: ', email)
+    console.log('first: ', firstname, 'lastname: ', lastname);
+    console.log('id: ', id);
     const url = 'dallinparker.me'
     const msg = {
-        to: email,
+        to: 'dallin.r.parker@gmail.com',
         from: 'vestid.email@gmail.com',
         subject: 'Vestid Support: Password Reset Request',
         text:`hi ${firstname}, you are getting this email because you have requested to reset your password by clicking: dallinparker.me`,
