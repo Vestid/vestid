c56f6d3357c23fb95155ccb827be63a4d5e1190b
Dallin Parker <dallin.r.parker@gmail.com>
2017-12-20 22:14:25 -0500

    flow for send-grid is working

--- public/app/components/pwreset/pwresetCtrl.js
+++ public/app/components/pwreset/pwresetCtrl.js
@@ -1,6 +1,7 @@
 angular.module('vestid').controller('pwresetCtrl', ['$scope', 'authService', ($scope, authService) => {
 
     $scope.resetPW = email => {
+        console.log('email: ', email)
         authService.resetPassword(email).then(res => {
             console.log('sendgrid res: ', res);
         })
--- server/sendgrid/index.js
+++ server/sendgrid/index.js
@@ -7,13 +7,18 @@ exports.sendReset = (req, res, next) => {
     //TODO: CREATE HELPER FUNCTION FOR MAKING THIS CLEANER && Create frontend url for reset input
     //TODO: generate token for URL
     const { id, firstname, lastname, email } = req.body.confirmed[0]
+    console.log('id: ', id)
+    console.log('first: ', firstname)
+    console.log('lastname: ', lastname)
+    console.log('email: ', email)
+    // const url = 'vestid.co/reset/approved/'
     const url = 'dallinparker.me'
     const msg = {
-        to: 'dallin.r.parker@gmail.com',
+        to: email,
         from: 'vestid.email@gmail.com',
         subject: 'Vestid Support: Password Reset Request',
-        text:`hi ${firstname}, you are getting this email because you have requested to reset your password by clicking: dallinparker.me`,
-        html: `<strong>hi ${firstname}, you're getting this email because you've requested to reset your password by clicking: ${url}</stong>`,
+        text:`Hi ${firstname}, you are getting this email because you have requested to reset your password by clicking: dallinparker.me`,
+        html: `<strong>Hi ${firstname}, you're getting this email because you've requested to reset your password by clicking: ${url}</stong>`,
     };
     sendgrid.send(msg);
 };
